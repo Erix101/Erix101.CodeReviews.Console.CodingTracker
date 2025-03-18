a1f3fcc2d99efe628adbc02ead55d52dd569bddc
Erika Hall <findingerix@hotmail.com>
2025-03-18 19:25:18 +0000

    first push

---
+++ Erix101.CodingTracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Erix101.CodingTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My coding tracker project coming soon!");
+        }
+    }
+}
